94900618942ab968c9426e28daf580bb9e85cdfa
TinHo <Honguyenthanhtin17@gmail.com>
2023-05-30 16:45:15 +0700

    no message

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,16 @@ import http from 'http';
 import {Server} from '@colyseus/core';
 import { WebSocketTransport } from "@colyseus/ws-transport";
 import {KhoKheRoom} from './Room/KhoKheRoom';
-import {createConnection} from 'typeorm';
-import * as dotenv from 'dotenv';
 
-dotenv.config();
 const port = Number(3000);
-const server = http.createServer(app);
+const server = http.createServer();
 const GameServer = new Server({
   transport: new WebSocketTransport({
     server,
   })
 });
 
-createConnection().then(async connection=>{
-   console.log('TypeOrm With Mongodb');
 
-   GameServer.define("KhoKhe",KhoKheRoom);
+GameServer.define("KhoKhe",KhoKheRoom);
 
-   GameServer.listen(port).then(()=>console.log("connected to port:"+port));
- }).catch(err=>{
-   console.log(err);
- });
-
+GameServer.listen(port).then(()=>console.log("connected to port:"+port));
